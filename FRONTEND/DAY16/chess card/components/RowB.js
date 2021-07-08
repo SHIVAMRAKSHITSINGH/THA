fc07a9853a313c1e98f1baad714363703e8f66ff
SHIVAM RAKSHIT SINGH <85128041+Toxin9598@users.noreply.github.com>
2021-07-08 23:30:43 +0530

    Add files via upload

---
+++ FRONTEND/DAY16/chess card/components/RowB.js
@@ -0,0 +1,19 @@
+  
+const RowB = () => {
+    return (
+      <>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+        <p className="black"></p>
+      </>
+    );
+  };
+  
+  export default RowB;
---
+++ FRONTEND/DAY16/chess card/components/RowW.js
@@ -0,0 +1,19 @@
+  
+const RowW = () => {
+    return (
+      <>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+        <p className="white"></p>
+      </>
+    );
+  };
+  
+  export default RowW;
