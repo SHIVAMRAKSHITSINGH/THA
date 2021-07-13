0f7798bd0c9313d874eca362bf2a59ff6872dfa4
SHIVAM RAKSHIT SINGH <85128041+Toxin9598@users.noreply.github.com>
2021-07-13 23:41:25 +0530

    Add files via upload

---
+++ FRONTEND/DAY19/src/App.js
@@ -0,0 +1,21 @@
+  
+import React, { Component } from "react";
+import './style.css';
+
+class App extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <>
+        <button className="inc" onClick={this.increment}>{this.state.count}</button>
+      </>
+    );
+  }
+}
+
+export default App;
