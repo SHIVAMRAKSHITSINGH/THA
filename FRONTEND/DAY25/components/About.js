671600cccf7d58282035dd79881e95ca00bb7750
SHIVAM RAKSHIT SINGH <85128041+Toxin9598@users.noreply.github.com>
2021-08-26 21:05:37 +0530

    Add files via upload

---
+++ FRONTEND/DAY25/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1 style={{fontSize: '30px',padding: "20px"}}>Hey its Toxin!!</h1>
+            </div>
+        )
+    }
+}
---
+++ FRONTEND/DAY25/components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div style={{textAlign: 'center'}}>
+                <h1 style={{fontSize: '30px',padding: "20px"}}>YEAH BOIS TOXIN IS AT PEAK.</h1>
+            </div>
+        )
+    }
+}
